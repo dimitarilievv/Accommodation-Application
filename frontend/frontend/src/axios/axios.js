e827dc46730223f31c1c4f6533664c2e0f936208
Dimitar I <dimitar.iliev123@gmail.com>
2025-06-27 14:18:35 +0200

    Changes in Dockerfile in frontend 8

--- frontend/frontend/src/axios/axios.js
+++ frontend/frontend/src/axios/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
-
+const baseURL = import.meta.env.MODE === 'development'
+    ? 'http://localhost:8181'
+    : '/api';
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8181", //with docker-compose
+    baseURL: baseURL, //with docker-compose
     //baseURL: "/api", //with kubernetes
     headers: {
         "Content-Type": "application/json",
