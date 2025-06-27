a3a5c01cb0e4b5dd79fecb3b5763c35091dd0168
Dimitar I <dimitar.iliev123@gmail.com>
2025-06-27 14:55:28 +0200

    fix: update axios baseURL for production

--- frontend/frontend/src/axios/axios.js
+++ frontend/frontend/src/axios/axios.js
@@ -3,8 +3,9 @@ const baseURL = import.meta.env.MODE === 'development'
     ? 'http://localhost:8181'
     : '/api';
 const axiosInstance = axios.create({
-    baseURL: baseURL, //with docker-compose
+    //baseURL: 'http://localhost:8181, //with docker-compose
     //baseURL: "/api", //with kubernetes
+    baseURL: "https://accommodation-backend.onrender.com", //with deployment
     headers: {
         "Content-Type": "application/json",
     },
