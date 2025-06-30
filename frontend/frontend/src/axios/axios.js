3db173a471ead17a815dcab267cb3729fa945761
Dimitar I <dimitar.iliev123@gmail.com>
2025-06-30 21:06:00 +0200

    Changed axios

--- frontend/frontend/src/axios/axios.js
+++ frontend/frontend/src/axios/axios.js
@@ -1,11 +1,23 @@
 import axios from "axios";
-const baseURL = import.meta.env.MODE === 'development'
-    ? 'http://localhost:8181'
-    : '/api';
+// const baseURL = import.meta.env.MODE === 'development'
+//     ? 'http://localhost:8181'
+//     : '/api';
+let baseURL = "";
+
+const hostname = window.location.hostname;
+
+if (hostname === "localhost") {
+    baseURL = "http://localhost:8181";
+} else if (hostname === "accommodation.local") {
+    baseURL = "/api";
+} else {
+    baseURL = "https://accommodation-backend.onrender.com";
+}
 const axiosInstance = axios.create({
     //baseURL: 'http://localhost:8181, //with docker-compose
     //baseURL: "/api", //with kubernetes
-    baseURL: "https://accommodation-backend.onrender.com", //with deployment
+    //baseURL: "https://accommodation-backend.onrender.com", //with deployment
+    baseURL,
     headers: {
         "Content-Type": "application/json",
     },
